615a411e52d115c435bef0dcfc92422b6e93bf76
Interface0625 <karolis.lukosius@gmail.com>
2014-09-23 10:14:47 +0200

    clean up

--- js/script.js
+++ js/script.js
@@ -1,51 +1,60 @@
-// Directory List
 function xhrGet(reqUri, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", reqUri, true);
-    xhr.onload = callback;
-    xhr.send();
-}
+	    var xhr = new XMLHttpRequest();
+	    xhr.open("GET", reqUri, true);
+	    xhr.onload = callback;
+	    xhr.send();
+	}
+	function openCode(url){
+			console.log("fetching gile" + url);
 
-var DirectoryList = {
-	ls: function(path, callback){
-		xhrGet(path, function(){
-			callback(this.responseText);
+		if(!url) return;
+		xhrGet(url, function(){
+			cm.doc.setValue(this.responseText);
+		})
+	}
+
+
+
+	var init = function(){
+		console.log("hej");
+		var treeData = [];
+		$(function() {
+	  		$('#container').jstree({ 'core' : {
+	  		 	"data" : function(o, cb){
+	  		 		var self = this;
+	  		 		xhrGet("data/folder_list.json", function(){
+	  		 			treeData = JSON.parse(this.responseText)
+	  		 			cb.call(
+	  		 				self, 
+	  		 				treeData
+              			);
+	  		 		});
+	  			}
+	  		} });
+
+
+
+	  		///
+			$('#container')
+			  // listen for event
+			  .on('changed.jstree', function (e, data) {
+			   	//1console.log(data);
+			   	//1console.log(e);
+			   	for(var i = 0; i < treeData.length; i++){
+			   		var id = data.selected[0];
+			   		if(treeData[i]["id"] === id){
+			   			var url = treeData[i]["url"];
+			   				console.log(treeData[i]["url"])
+			   			openCode(url);
+			   		}
+			   	}
+			  })
 		});
+		//
+		cm = CodeMirror.fromTextArea(
+			document.getElementById("code"), {
+        		lineNumbers: true,
+        		mode: "javascript",
+        		theme: "mbo"
+    	});
 	}
-
-};
-
-var View = {
-	makeList: function(obj){
-		for ( key in obj ){
-			console.log(key);
-			console.log(obj[key]);
-		}
-	}
-};
-
-
-makeFileDiv = function(path){
-	var div = document.createElement("div");
-	div.class = "file";
-	
-	var img = document.createElement("img");
-	img.src = "images/file.png";
-	img.class = "fileIcon";
-	
-	var p = document.createElement("p");
-	p.innerText = "path";
-
-	var container = document.getElementById("fileContainer");
-
-	div.appendChild(img);
-	div.appendChild(p);
-	container.appendChild(div);
-}
-
-// Tests
-//test = DirectoryList
-/*test.ls("/ls?/", function(args){
-	console.log(args);
-	View.makeList(JSON.parse(args));
-});*/
